41ee6fd29f4c556147de0f944aa7359144bc2a25
thartet <nolwenn.taton@utt.fr>
2023-05-12 14:11:08 +0200

    add-first commit

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# compression-image
-Script de compression d'images en bash 
+# Image compression script 
+This script is used to reduce the image size and optimize the number of colors of the image.
+## Installation guide
+You first have to clone the repository to obtain the script locally.
+To run the script you need to install two libraries : *pngquant* and *ImageMagick*. <br><br>
+### Install pngquant : <br>
+Update the package list : 
+```bash
+sudo apt update
+```
+Run the command to install the library : 
+```bash
+sudo apt install -y pngquant
+```
+You can check the version to verify that the library was installed : 
+```bash
+pngquant --version
+```
+### Install ImageMagick
+It is highly possible that *ImageMagick* is already installed on your computer, since many softwares use it as a dependency. You can verify the installation with the following command line :
+```bash
+convert -version
+```
+Run the command to install the software :
+```bash
+sudo apt install -y imagemagick
+```
+You can check the version to verify that the library was installed : 
+```bash
+convert -version
+```
+<br>
+Then you can run the script with the images that you want to compress. <br>
+
+```bash
+bash compress.sh <file>
+```
+
+***You can only use this script to compress png, jpeg and webp files !***
+
+## To do
+- [x] Reduction of the number of colors of the image.
+- [x] Implementation of different treatments for png, jpeg and webp files.
+- [x] Reduction of the quality of the image.
+- [x] Usage with -h option
+- [ ] Modify the script so that it could be implemented as a plugin on a web site.
+- [ ] Gestion of the codes returned by the script.
+- [ ] Adaptative compression (low, medium and high mode)
---
+++ compress.sh
@@ -0,0 +1,106 @@
+#!/bin/sh
+
+# Copyright (C) 2023 thartet https://github.com/thartet
+# This script is used to reduce the image size and optimize the number of colors of the image.
+
+function pngTreatment {
+
+    name=$(echo "$1" | sed 's/\.png$//')
+    color_number=$(identify -format %k "$1")
+
+    if [ $color_number -gt 256 ]
+    then
+        # Reduce the number of colors in the PNG using pngquant
+        pngquant --quality=0-100 --force --speed=1 --strip --output "/tmp/coloured_$1" --verbose 256 -- "$1" 
+    fi
+    
+    # Reduce the file size of the picture using ImageMagick
+    convert "/tmp/coloured_$1" -strip -quality 65% "reduced_$name.webp"
+
+    # Remove the png file
+    rm "/tmp/coloured_$1"
+    echo "Reduction of $1 complete. The new file is reduced_$name.webp"
+
+}
+
+function jpgTreatment {
+    name=$(echo "$1" | sed 's/\.jpg$//')
+    color_number=$(identify -format %k "$1")
+
+    if [ $color_number -gt 256 ]
+    then
+        #Reduce the number of colors in the PNG using ImageMagick
+        convert "$1" -strip -posterize 256 "/tmp/coloured_$1"
+    fi
+
+    # Reduce the file size of the picture using ImageMagick
+    convert "/tmp/coloured_$1" -strip -quality 65% "reduced_$name.webp"
+
+    # Remove the jpeg file
+    rm "/tmp/coloured_$1"
+    echo "Reduction of $1 complete. The new file is reduced_$name.webp"
+}
+
+function webpTreatment {
+    name=$(echo "$1" | sed 's/\.webp$//')
+    color_number=$(identify -format %k "$1")
+
+    if [ $color_number -gt 256 ]
+    then
+        #Reduce the number of colors in the webp using ImageMagick
+        convert "$1" -strip -posterize 256 "/tmp/coloured_$1"
+    fi
+
+    # Reduce the file size of the picture using ImageMagick
+    convert "/tmp/coloured_$1" -strip -quality 65% "reduced_$name.webp"
+    rm "/tmp/coloured_$1"
+    echo "Reduction of $1 complete. The new file is reduced_$name.webp"
+}
+
+# Check the number of arguments
+if [ $# -ne 1 ]
+then
+    echo "The number of arguments is not correct"
+    exit 1
+fi
+
+# Help message
+if [ $1 == "-h" ]
+then
+    echo "This script is used to reduce the image size and optimize the number of colors of the image."
+    echo "Usage: img2.sh <file>"
+    echo "Supported types are: .png, .jpeg, .webp, .svg"
+    exit 0
+fi
+
+# Check if the file exists
+if [ ! -f $1 ]
+then
+    echo "The file does not exist"
+    exit 1
+fi
+
+# Treat the file according to its extension
+case $(file --extension -b $1) in
+
+    "png") pngTreatment $1;;
+
+    "jpeg/jpg/jpe/jfif") jpgTreatment $1;;
+
+    "webp") webpTreatment $1;;
+
+    "svg") echo "The file is a svg file. No treatment is needed.";return 0;;
+
+    *) echo "The type of your file is not supported. \n Supported types are: .png, .jpeg, .webp, .svg";return 1;;
+
+esac
+
+# Calculate the pourcentage of reduction
+old_size=$(stat -c%s "$1")
+new_size=$(stat -c%s "reduced_$name.webp")
+
+echo "The old size is $old_size"
+echo "The new size is $new_size"
+
+percentage=$((($old_size-$new_size)*100/$old_size))
+echo "The file size has been reduced by $percentage%"
